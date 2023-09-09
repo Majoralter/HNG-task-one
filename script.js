4f26cedda8b5e594b937c7e1f467683008be7151
Jenrola Jackson <jacksinsthegenius@gmail.com>
2023-09-09 15:22:49 +0100

    update script.js

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const currentDay = document.querySelector('.current--day'),
       currentTime = document.querySelector('.current--time'),
       date = new Date(),
       days = date.toUTCString().split(","),
-      day = `${days[0]}day`
+      day = days[0] === 'Sat' ? `${days[0]}urday` : days[0] === 'Tue' ? `${days[0]}sday` : days[0] === 'Wed' ? `${days[0]}nesday` : days[0] === 'Thu' ? `${days[0]}rsday` : `${days[0]}day`
 
 
       const returnCurrentTime = () =>{
-        let h = date.getUTCHours().toString().length < 2 ? `0${date.getUTCHours()}` : date.getUTCHours(),
+        const date = new Date(),
+        h = date.getUTCHours().toString().length < 2 ? `0${date.getUTCHours()}` : date.getUTCHours(),
         m = date.getUTCMinutes().toString().length < 2 ? `0${date.getUTCMinutes()}` : date.getUTCMinutes(),
         s = date.getUTCSeconds().toString().length < 2 ? `0${date.getUTCSeconds()}` : date.getUTCSeconds(),
         ms = date.getUTCMilliseconds().toString().length < 3 ? `0${date.getUTCMilliseconds()}` : date.getUTCMilliseconds()
@@ -16,5 +17,4 @@ const currentDay = document.querySelector('.current--day'),
 
 currentDay.textContent = `Current day: ${day}`
 
-window.addEventListener("load", returnCurrentTime())
 setInterval(returnCurrentTime, 1000)
